1944fc911ac3ae56a3e14fc1bafb034dd131e304
Ilya Alekseenkov <rmfighht@yandex.ru>
2021-08-24 19:14:19 +0300

    Выполнил задание 2 лекции 6

--- src/js/__tests__/test-app.js
+++ src/js/__tests__/test-app.js
@@ -1,109 +1,59 @@
-import Bowerman from '../Bowman';
-import Swordsman from '../Swordsman';
-import Magician from '../Magician';
-import Undead from '../Undead';
-import Zombie from '../Zombie';
-import Daemon from '../Daemon';
-import Character from '../Character';
+import extractArrayWithPowerSkills from '../app';
 
-test('create the character "Bowman"', () => {
-  const expected = {
-    attack: 25,
-    defence: 25,
-    health: 100,
-    level: 1,
-    name: 'Anatoly',
+test('Correct array extraction', () => {
+  const expected = [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      description: 'Описание недоступно',
+    },
+  ];
+
+  const character = {
+    name: 'Лучник',
     type: 'Bowman',
-  };
-
-  const received = new Bowerman('Anatoly');
-
-  expect(received).toEqual(expected);
-});
-
-test('create the character "Swordsman"', () => {
-  const expected = {
+    health: 50,
+    level: 3,
     attack: 40,
     defence: 10,
-    health: 100,
-    level: 1,
-    name: 'Anatoly',
-    type: 'Swordsman',
+    special: [
+      {
+        id: 8,
+        name: 'Двойной выстрел',
+        icon: 'http://...',
+        description: 'Двойной выстрел наносит двойной урон',
+      },
+      {
+        id: 9,
+        name: 'Нокаутирующий удар',
+        icon: 'http://...',
+      },
+    ],
   };
 
-  const received = new Swordsman('Anatoly');
+  const received = extractArrayWithPowerSkills(character);
 
   expect(received).toEqual(expected);
 });
 
-test('create the character "Magician"', () => {
-  const expected = {
-    attack: 10,
-    defence: 40,
-    health: 100,
-    level: 1,
-    name: 'Anatoly',
-    type: 'Magician',
-  };
-
-  const received = new Magician('Anatoly');
-
-  expect(received).toEqual(expected);
-});
-
-test('create the character "Undead"', () => {
-  const expected = {
-    attack: 25,
-    defence: 25,
-    health: 100,
-    level: 1,
-    name: 'Anatoly',
-    type: 'Undead',
-  };
-
-  const received = new Undead('Anatoly');
-
-  expect(received).toEqual(expected);
-});
-
-test('create the character "Zombie"', () => {
-  const expected = {
+test('If there is no array in the object', () => {
+  const character = {
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 3,
     attack: 40,
     defence: 10,
-    health: 100,
-    level: 1,
-    name: 'Anatoly',
-    type: 'Zombie',
   };
 
-  const received = new Zombie('Anatoly');
+  const received = extractArrayWithPowerSkills(character);
 
-  expect(received).toEqual(expected);
-});
-
-test('create the character "Daemon"', () => {
-  const expected = {
-    attack: 10,
-    defence: 40,
-    health: 100,
-    level: 1,
-    name: 'Anatoly',
-    type: 'Daemon',
-  };
-
-  const received = new Daemon('Anatoly');
-
-  expect(received).toEqual(expected);
-});
-
-test('uncorrect name character', () => {
-  expect(() => new Daemon('A')).toThrowError(
-    new Error('Длина имени должна быть не менее 2-ух и не более 10-ти символов'),
-  );
-});
-
-test('uncorrect type character', () => {
-  expect(() => new Character('Anatoly', 'Druid')).toThrowError(
-    new Error('Некорректный тип персонажа'),
-  );
+  expect(received).toEqual(null);
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,29 @@
-import Bowman from './Bowman';
-import Swordsman from './Swordsman';
-import Magician from './Magician';
-import Undead from './Undead';
-import Zombie from './Zombie';
-import Daemon from './Daemon';
+export default function extractArrayWithPowerSkills(obj) {
+  const { special } = obj; // извлекаем массив special в одноименную переменную
 
-// ? Создаём персонажей:
-const bowman = new Bowman('Anatoly', 'Bowman');
-const swordsman = new Swordsman('Vasily', 'Swordsman');
-const magician = new Magician('Ivan', 'Magician');
-const undead = new Undead('Dmitriy', 'Undead');
-const zombie = new Zombie('Aleksey', 'Zombie');
-const daemon = new Daemon('Ilya', 'Daemon');
+  // ? Если special нет:
+  if (!special) {
+    return null;
+  }
 
-// ? Чисто глянуть, что мы создали:
-// eslint-disable-next-line no-console
-console.log(bowman, swordsman, magician, undead, zombie, daemon);
+  // ? Если special есть:
+  const arr = [];
+
+  for (const elem of special) {
+    const {
+      id,
+      name,
+      icon,
+      description = 'Описание недоступно',
+    } = elem;
+
+    arr.push({
+      id,
+      name,
+      icon,
+      description,
+    });
+  }
+
+  return arr;
+}
